1d43317561ae09c79a65673aef466f4ba5543db7
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-06-08 00:01:10 +0300

    Linking Javascript

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,11 @@
+//JAVSCRIPT TEST CODE
+/*
+let reverseWord = "Beatrice";//Function To Reverse and Test String and JS functionality
+function reversingWord(str) {
+  let wordReversed =
+    typeof str === "string"
+      ? str.split("").reverse().join("")
+      : console.log("Word Not String");
+  return wordReversed;
+}
+console.log(reversingWord(reverseWord));*/
