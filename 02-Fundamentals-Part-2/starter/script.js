8a18fbbabd57c6c6c420bd754757510017af1070
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-06-12 13:30:16 +0300

    Function Expressions vs Function Declarations

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -18,3 +18,19 @@ console.log("AppleJuice", appleJuice);
 
 const appleOrangeJuice = fruitProcessor(5, 2);
 console.log("Apple x OrangeJuice", appleOrangeJuice);*/
+
+// Function Declaration Vs Function Expressions
+
+function calcAge(birthYear) {
+  //Function Declaration
+  return 2037 - birthYear;
+}
+const ageOne = calcAge(2003);
+console.log(ageOne);
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const age2 = calcAge2(2007);
+console.log(ageOne, age2);
