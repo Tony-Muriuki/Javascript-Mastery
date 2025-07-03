1d9d9f96aadd5ccfd46c7cec35ddf2b71a16ad0f
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-07-04 00:08:00 +0300

    Maps

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -100,3 +100,5 @@ const mexicanFoods = new Set([
 ]);
 
 // MAPS a map is a built in Javascript Datastructure consisiting of key value pairs similar to objects however there are some key differences
+
+const map = new map();
