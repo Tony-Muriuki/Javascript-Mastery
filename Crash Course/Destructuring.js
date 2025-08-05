546a74acd8aeddbe17e0be74a06ed5ac5cfd8a27
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-08-05 03:11:48 +0300

    Destructuring ES6

--- Crash Course/Destructuring.js
+++ Crash Course/Destructuring.js
@@ -11,9 +11,40 @@ const info = {
 };
 
 // Traditional way of accessing values from an object
-console.log(info.fname + " " + info.sname);
+// console.log(info.fname + " " + info.sname);
 
 // Using Destructuring
 
-const { fname: firstName, sname: surName, idNo } = info;
-console.log(firstName);
+// const { fname: firstName, sname: surName, idNo } = info;
+// console.log(firstName);
+
+// Default Values in Array Destructuring
+
+// const [element1 = defaultValue1, element2 = defaultValue2] = array;
+// const numbers = [1, 2];
+
+// Using Default Values with undefined Elements:
+
+// const numbers = [1, undefined];
+
+// const [first, second = 20] = numbers;
+
+// console.log(first); // 1
+// console.log(second); // 20
+
+// 2. Objects default destructuring
+
+// syntax : const { property1 = defaultValue1, property2 = defaultValue2 } = object;
+
+const user = {
+  name: "Alice",
+  age: 25,
+};
+
+const { name: userName, age: userAge, country: userCountry = "Unknown" } = user;
+
+console.log(userName); // Alice
+console.log(userAge); // 25
+console.log(userCountry); // Unknown
+
+// Nested Destructuring
