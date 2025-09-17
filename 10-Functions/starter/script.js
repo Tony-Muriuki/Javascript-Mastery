dc1485d35e3db5b08c23a7e8623bd57b577f3bc2
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-17 22:58:46 +0300

    Argument Passing in JavaScript

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -20,3 +20,8 @@ const createBooking = function (flightNum, numPassengers = 1, price = 199) {
 };
 createBooking('LH123');
 createBooking('LH123', 2, 800);
+
+// Lecture 136: Argument Passing in JavaScript
+
+const flight = 'LH234'; // primitive (string)
+const jonas = { name: 'Jonas Schmedtmann', passport: 24739479284 }; // object
