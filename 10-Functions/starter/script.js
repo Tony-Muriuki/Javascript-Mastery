8b586e744c99bf838dc6d9d987f8880e5aa71be6
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-18 00:55:16 +0300

    Argument Passing in JavaScript

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -22,6 +22,46 @@ createBooking('LH123');
 createBooking('LH123', 2, 800);
 
 // Lecture 136: Argument Passing in JavaScript
-
+console.log('------------------------------------------------------------');
 const flight = 'LH234'; // primitive (string)
 const jonas = { name: 'Jonas Schmedtmann', passport: 24739479284 }; // object
+
+// Checkin Function
+const Checkin = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'MR' + passenger.name;
+  if (passenger.passport === 24739479284) {
+    console.log('Checkin');
+  } else {
+    console.log('Wrong Passport');
+  }
+};
+Checkin(flight, jonas);
+console.log(flight);
+console.log(jonas);
+
+let person = { name: 'Alice', age: 25 };
+
+function changePerson(myPerson) {
+  myPerson.age = 30; // mutate property
+  console.log('Inside function:', myPerson);
+}
+
+changePerson(person);
+console.log('Outside function:', person);
+
+const Tony = {
+  firstName: 'Tony',
+  lastName: 'Kamande',
+  girlFriend: 'Beatrice',
+  status: 'Unsaved',
+};
+
+function bornAgain(obj) {
+  obj.status = 'Saved';
+  console.log('Inside Function', obj);
+}
+bornAgain(Tony);
+console.log('Outside Function', Tony);
+
+// The resaon as to why it changed is because objects are reference types and when passed to functions as arguements they are passed as references to the objects location in memory. Meaning changes inside the function affect the original.
