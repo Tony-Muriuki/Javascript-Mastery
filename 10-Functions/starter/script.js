d435ce5e0923f4e740a66dd538bb855ca3fd37e2
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-17 22:51:32 +0300

    Default Parameters

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,3 +1,22 @@
 'use strict';
 
 //Lecture 129 Default Parameters.
+
+/*
+--Defination : Default parameters in functions let us assign fallback values to arguments when none are provided, making functions more flexible and concise.
+
+
+*/
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 199) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+createBooking('LH123');
+createBooking('LH123', 2, 800);
