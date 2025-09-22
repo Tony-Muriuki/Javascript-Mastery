5a59111724910ace16df2cee57e90868a3267338
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-23 01:55:43 +0300

    OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -3,3 +3,5 @@
 // OBJECT ORIENTED PROGRAMMING.
 
 // Object oriented programming is a just but a programming paradigm (style of writing code) that is based on objects. Obhects can contain data which we call properties and code which we call methods . In OOP objects group data + code/behaviour into one self contained block.
+
+// A class is a blueprint for creating objects. An object created from a class is an instances and we can have multiple instances created from a class .
