fc1b6ad5eeabe45f40330d7cd22a83007b243231
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-23 01:33:52 +0300

    OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,5 @@
 'use strict';
+
+// OBJECT ORIENTED PROGRAMMING.
+
+// Object oriented programming is a just but a programming paradigm (style of writing code) that is based on objects. Obhects can contain data which we call properties and code which we call methods . In OOP objects group data + code/behaviour into one self contained block.
