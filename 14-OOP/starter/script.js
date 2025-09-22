67224479b00fb413f5ba72b9381e70f7ff5b148d
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-23 02:21:01 +0300

    Abstraction

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -5,3 +5,10 @@
 // Object oriented programming is a just but a programming paradigm (style of writing code) that is based on objects. Obhects can contain data which we call properties and code which we call methods . In OOP objects group data + code/behaviour into one self contained block.
 
 // A class is a blueprint for creating objects. An object created from a class is an instances and we can have multiple instances created from a class .
+
+// There are 4 fundamental principles that can guide us towrds a good class implementation which are Abstraction, Polymorphism, Encapsulation ,and Inheritance.
+
+// Abstraction
+//So abstraction is one of the fundamentals of OOP in that it ignores or hides details that dont matter providing a simplified view(blackBox)eg: Phone analogy: Users see a screen and buttons while the other detailings like motherboard , chips , phone temperature and voltage are abstracted.
+
+// 2:
