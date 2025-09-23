7b17b9954771adc4d2e7a68b533cb90614c4da00
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-23 16:00:06 +0300

    OOP

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -5,11 +5,18 @@
 // Constructor Functions and The New Operator
 
 const Person = function (firstName, birthYear) {
+  // Instance Properties
   this.firstName = firstName;
   this.birthYear = birthYear;
+
+  // Adding a method Never do This
+  // this.calcAge = function () {
+  //   console.log(2037 - this.birthYear);
+  // };
 };
 
 const jonas = new Person('Jonas', 1991);
 const mary = new Person('Mary', 991);
+
 console.log(jonas instanceof Person);
 console.log(mary instanceof Person);
