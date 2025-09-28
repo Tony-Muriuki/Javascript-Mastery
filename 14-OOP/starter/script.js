e178047f9f0b87da30e7da0196b5f7c8238836eb
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-29 00:23:28 +0300

    Challenge

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -115,3 +115,54 @@ console.log(Object.prototype.__proto__ === null);
 const sarah = Object.create(PersonProt0);
 sarah.init('Sarah', 1979);
 sarah.calcAge();
+
+/* 
+1. Use a constructor function to implement a Car. A car has a make and a speed property. The speed property is the current speed of the car in km/h;
+2. Implement an 'accelerate' method that will increase the car's speed by 10, and log the new speed to the console;
+3. Implement a 'brake' method that will decrease the car's speed by 5, and log the new speed to the console;
+4. Create 2 car objects and experiment with calling 'accelerate' and 'brake' multiple times on each of them.
+
+DATA CAR 1: 'BMW' going at 120 km/h
+DATA CAR 2: 'Mercedes' going at 95 km/h
+
+GOOD LUCK 😀
+*/
+// Coding Challenge #2
+
+/* 
+1. Re-create challenge 1, but this time using an ES6 class;
+2. Add a getter called 'speedUS' which returns the current speed in mi/h (divide by 1.6);
+3. Add a setter called 'speedUS' which sets the current speed in mi/h (but converts it to km/h before storing the value, by multiplying the input by 1.6);
+4. Create a new car and experiment with the accelerate and brake methods, and with the getter and setter.
+
+DATA CAR 1: 'Ford' going at 120 km/h
+
+GOOD LUCK 😀
+*/
+
+//Es6 Class
+
+class Car {
+  constructor(make, speed) {
+    this.make = make;
+    this.speed = speed;
+  }
+  //Accelerate Methods
+  accelerate() {
+    const acceleratedSpeed = this.speed + 10;
+    console.log(acceleratedSpeed);
+  }
+  //Brake Method
+  brake() {
+    const decreasedSpeed = this.speed - 5;
+    console.log(decreasedSpeed);
+  }
+  //Getter Func speedUS
+  get speedUS() {}
+  //Setter Func speedUS
+  set speedUS(speed) {}
+}
+const ford = new Car('Ford', 120);
+console.log(ford);
+//Accelerated Speed
+ford.accelerate();
