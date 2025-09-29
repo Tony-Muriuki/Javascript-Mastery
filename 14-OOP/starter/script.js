d96a309cab28d29bfffbc0fdd1a30a7ebf05e742
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-09-30 00:00:51 +0300

    Classes

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -257,3 +257,36 @@ class StudentCl extends Personcl {
 const mikes = new StudentCl('Mikse Ross', 2000, 'Law');
 
 //Another Class Example
+class Account {
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.pin = pin;
+    this.movements = [];
+    this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account ${this.owner}`);
+  }
+  //Public Interface of our Objects
+  deposit(val) {
+    this.movements.push(val);
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+  }
+  approveLoan(val) {
+    return true; // just a stub, always approves
+  }
+  requestLoan(val) {
+    if (this.approveLoan(val)) {
+      this.deposit(val);
+      console.log('Loan approved');
+    }
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+acc1.deposit(250);
+acc1.withdraw(140);
+console.log(acc1);
