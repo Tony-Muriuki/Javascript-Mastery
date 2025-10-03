232958e798119bfca2c170332249a27e9283eaf7
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-10-03 15:32:11 +0300

    DOM MOdel

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,3 +4,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+//Loop
+for (let i = 0; i < btnsOpenModal.lenght; i++)
+  console.log(btnsOpenModal[i].textContent);
